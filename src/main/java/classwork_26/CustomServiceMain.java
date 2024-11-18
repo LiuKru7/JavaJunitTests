88698d6b4d25dec8bbc53cc9b15f90a219ffda95
liudvikaskruopis <liukru7@gmail.com>
2024-11-18 17:55:45 +0200

    streams and junit tests

--- src/main/java/classwork_26/CustomServiceMain.java
+++ src/main/java/classwork_26/CustomServiceMain.java
@@ -1,17 +1,19 @@
 package classwork_26;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CustomServiceMain {
 
     public static void main(String[] args) {
+
         List<Customer> customers = new JsonService().generateCustomerData();
         CustomerService customerService = new CustomerService();
+
         System.out.println("Customers: " + customerService.getAllCustomersWithSpecificCity(customers, "wonderland") );
-
-
-
+        System.out.println("Most expensive product: " + customerService.getMostExpensiveProduct(customers).getName());
+        System.out.println("Quantity of all products: " + customerService.getQuantityOfAllProducts(customers));
+        System.out.println("Products: " + customerService.getAllProductsBySpecificAge(customers, 25, 35));
+        System.out.println("Customers: " + customerService.getCustomersListWhoSpecificProduct(customers, "Book"));
 
     }
 }
